ba6011e33363550f1fb3bbb1971028669e3a855c
Victor Hu <vhu@us.ibm.com>
2016-01-20 12:48:57 -0500

    Add in scripts that will backup the xcat.org important files so that in the event we lose the xcat.org server, it will not be very difficult to restore the files

---
+++ backup_xcat.org.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# This script is copied over to xcat.org and 
+# executed to tar up the directories in the list below
+
+SAVE_DIRECTORIES="./css ./images ./*.html ./files"
+
+DATE=`date +%Y%M%d`
+FILENAME="${DATE}-xcat.org-backup.tar.gz"
+
+TARGET_TOPDIR=/var/www/xcat.org
+
+# -------------- starting the backup process --------------
+
+echo "Creating backup file ${FILENAME}"
+
+cd ${TARGET_TOPDIR}
+tar -czvf $FILENAME ${SAVE_DIRECTORIES}
+
+# move the backup to the home directory of the user
+mv ${TARGET_TOPDIR}/${FILENAME} ~/
+
+# list out for debug 
+ls -ltr ~/${FILENAME}
+
